f031439904553d0bdecc357b5b389d3f04a95d17
TKSkan <ahighring@gmail.com>
2017-08-15 14:55:25 +0900

    first file upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+phina.globalize();
+
+phina.define('MainScene', {
+    superClass: 'CanvasScene',
+    init: function() {
+        this.superInit();
+        // 背景色を指定
+        this.backgroundColor = '#444';
+        // ラベルを生成
+        this.label = Label('Hello, phina.js!').addChildTo(this);
+        this.label.x = this.gridX.center(); // x 座標
+        this.label.y = this.gridY.center(); // y 座標
+        this.label.fill = 'white'; // 塗りつぶし色
+    },
+});
+
+// メイン処理
+phina.main(function() {
+    // アプリケーション生成
+    var app = GameApp({
+        startLabel: 'main', // メインシーンから開始する
+    });
+    // アプリケーションの実行
+    app.run();
+})
